4a6862f5d24ae793659eb346796ae9cbcf4276e9
dev_Homero <herberthebm@gmail.com>
2021-10-02 16:00:54 -0600

    Agregando Clase de Prueba

---
+++ Models/prueba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba.Models
+{
+    public class prueba
+    {
+
+        public int Mensaje { get; set; }
+
+
+    }
+}
